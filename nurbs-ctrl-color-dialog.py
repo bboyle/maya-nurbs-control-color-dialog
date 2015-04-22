b1f5c2f6edfd5f271c601811290bd7210430e4b6
Ben Boyle <benjamins.boyle@gmail.com>
2015-04-23 00:51:41 +1000

    open color dialog

--- nurbs-ctrl-color-dialog.py
+++ nurbs-ctrl-color-dialog.py
@@ -1,7 +1,7 @@
 import maya.cmds as cmds
 
 # get all selected objects
-selectedObjects = cmds.ls(sl=True)
+# selected = cmds.ls(sl=True)
 
 # TODO filter list to nurbs?
 
@@ -9,6 +9,23 @@ selectedObjects = cmds.ls(sl=True)
 
 # ask for a colour
 # use the first selected objects color as the default
+# colorEditor([hsvValue=[float, float, float]], [mini=boolean], [parent=string], [position=boolean], [result=boolean], [rgbValue=[float, float, float]])
 
-# loop through all selections
-	# set the color
+result = cmds.colorEditor()
+buffer = result.split()
+# if a color was chosen
+if '1' == buffer[3]:
+	values = cmds.colorEditor(query=True, rgb=True)
+	print 'RGB = ' + str(values)
+	alpha = cmds.colorEditor(query=True, alpha=True)
+	print 'Alpha = ' + str(alpha)
+
+	# loop through all selections
+	# for o in selected:
+		# set the color
+		# print o.name
+
+else:
+	print 'Editor was dismissed'
+
+
